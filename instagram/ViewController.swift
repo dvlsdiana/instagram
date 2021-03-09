a276f2e5ea9f61ebb44449fddcef4cb328e46c7c
Diana Garcia <Diagarcia@csumb.edu>
2021-03-08 16:00:01 -0800

    Initial Commit

---
+++ instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instagram
+//
+//  Created by Diana Garcia Davalos on 3/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
